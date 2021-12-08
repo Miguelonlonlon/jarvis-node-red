97794e434d1e7f0a166d42e84e8e95e539f632de
Miguelonlonlon <botbotones@protonmail.com>
2021-12-08 12:14:16 +0100

    modified:   config.sh 	new file:   es/commands 	new file:   es_ES

--- config.sh
+++ config.sh
@@ -1,3 +1,3 @@
 pg_node_red_uri=http://127.0.0.1 # uri of node-red 
-pg_node_red_port=1880 # port of node-red 
+pg_node_red_port=8766 # port of node-red 
 pg_node_red_endpoint=jarvis # endpoint to process order through nod-red 
